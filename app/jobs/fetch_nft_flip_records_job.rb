30c6e956bd2f360d9320b55205077cfe1c65581a
Hazel <xp0729@gmail.com>
2022-05-16 18:10:30 +0800

    update fetch flip records job and update index view

--- app/jobs/fetch_nft_flip_records_job.rb
+++ app/jobs/fetch_nft_flip_records_job.rb
@@ -1,15 +1,9 @@
 class FetchNftFlipRecordsJob < ApplicationJob
   queue_as :daily_job
 
-  NFTS = ["bored-ape-kennel-club", "autoglyphs", "pegz", "azuki", "rtfkt-nike-cryptokicks",
-          "ikb-cachet-de-garantie", "bored-ape-yacht-club", "beanz-official", "colorglyphs",
-          "nouns", "tom-sachs-rockets", "wolfgamelegacy", "cryptopunks", "meebits",
-          "mutant-ape-yacht-club", "fvck-crystal", "10ktf", "cryptoadz", "gutter-cat-gang",
-          "mclarenmsolabgenesis", "otherdeed", "proof-moonbirds", "doodles"]
-
   def perform
-    NFTS.each do |slug|
-      FetchNftFlipDataByNftJob.perform_later(slug)
+    Nft.where.not(opensea_slug: nil).each do |nft|
+      FetchNftFlipDataByNftJob.perform_later(nft.opensea_slug)
     end
   end
 end
